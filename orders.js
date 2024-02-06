9ff3e305fd67e5b6e2831d0ae344ee600fbdeb57
Aryan <113187718+aryanjain762@users.noreply.github.com>
2024-02-06 15:20:25 +0530

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,7 @@
+const orders = [
+    { productName: 'Product A', productNumber: '12345', paymentStatus: 'In Progress', status: 'Completed' },
+    { productName: 'Product B', productNumber: '67890', paymentStatus: 'Due', status: 'Pending' }, 
+    { productName: 'Product C', productNumber: '45678', paymentStatus: 'Overdue', status: 'Failed' },
+    { productName: 'Product D', productNumber: '98765', paymentStatus: 'Due', status: 'Pending' },
+    { productName: 'Product E', productNumber: '54321', paymentStatus: 'Completed', status: 'Completed' }
+]
